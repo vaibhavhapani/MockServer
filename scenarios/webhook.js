97f49ada07dbd51d36c0c2791ebb9d0cdd566dab
vaibhavhapani <vaibhavhapani84@gmail.com>
2026-04-22 13:29:53 +0530

    add logging

--- scenarios/webhook.js
+++ scenarios/webhook.js
@@ -1,17 +1,26 @@
 // Webhook GET
 function handleWebhookVerification(req, res) {
+  console.log("---- Incoming GET /webhook ----");
+  console.log("Headers:", JSON.stringify(req.headers, null, 2));
+  console.log("Query:", JSON.stringify(req.query, null, 2));
+
   const challenge = req.query["hub.challenge"];
+  console.log("hub.challenge received:", challenge);
   if (challenge) {
     res.status(200).send(challenge);
   } else {
-    res.sendStatus(200);
+    console.log("Bad request. hub challange is missing")
+    res.sendStatus(400);
   }
 }
 
 // Webhook POST
 function handleWebhookEvent(req, res) {
+   console.log("---- Incoming POST /webhook ----");
+  console.log("Headers:", JSON.stringify(req.headers, null, 2));
   console.log("Event received:", JSON.stringify(req.body, null, 2));
   res.sendStatus(200);
 }
 
 module.exports = { handleWebhookEvent, handleWebhookVerification };
+ 
